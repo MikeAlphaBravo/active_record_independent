1980bc38029a8489546a31c8ac0f403c6c0c4f3a
Michael Brooks <mikeyabrooks@gmail.com>
2017-09-29 09:59:53 -0700

    restart file structure after db failure

---
+++ lib/spec_helper.rb
@@ -0,0 +1,26 @@
+ENV['RACK_ENV'] = 'test'
+
+require("bundler/setup")
+Bundler.require(:default, :test)
+set(:root, Dir.pwd())
+
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('./app')
+
+Dir[File.dirname(__FILE__] + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Shoe.all.each do |shoe|
+      shoe.destroy
+    end
+    Store.all.each do |store|
+      store.destory
+    end
+    Record.all.each do |record|
+      record.destory
+    end
+  end
+end
