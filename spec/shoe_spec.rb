cfb2c6c0b4c8a93b2e0f791ee770ca87db2775b2
Michael Brooks <mikeyabrooks@gmail.com>
2017-09-30 14:44:19 -0700

    add passing specs for character max and repeats

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,20 @@ require('spec_helper')
 
 describe(Shoe) do
   it { should have_many(:stores)}
+
+  it("ensures the length of brand is at most 100 characters") do
+    shoe = Shoe.new({:brand => "a".*(101)})
+    expect(shoe.save()).to(eq(false))
+  end
+
+  it("ensures that brand doesn't already exist") do
+    Shoe.create({:brand => 'Croc Larkins'})
+    shoe = Shoe.new({:brand => 'croc larkins'})
+    expect(shoe.save()).to(eq(false))
+  end
+
+  # it('will capitalize name') do
+  #   shoe = Shoe.create({:brand => 'croc larkins'})
+  #   expect(shoe.brand).to(eq('Croc Larkins'))
+  # end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,20 @@ require('spec_helper')
 
 describe(Store) do
   it { should have_many(:shoes)}
+
+  it("ensures the length of name is at most 100 characters") do
+    store = Store.new({:name => "a".*(101)})
+    expect(store.save()).to(eq(false))
+  end
+
+  it("ensures that name doesn't already exist") do
+    Store.create({:name => 'Jim\'s Shoes'})
+    store = Store.new({:name => 'Jim\'s Shoes'})
+    expect(store.save()).to(eq(false))
+  end
+
+  # it('will capitalize name') do
+  #   store = Store.create({:name => 'croc larkins'})
+  #   expect(store.name).to(eq('Croc Larkins'))
+  # end
 end
